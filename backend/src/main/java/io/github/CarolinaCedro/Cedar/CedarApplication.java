f8ac754083e512c5dc3bd59ea059de03d72ee9e8
CarolinaCedro <yotechgirl@gmail.com>
2022-09-07 17:33:59 -0300

    create project

---
+++ backend/src/main/java/io/github/CarolinaCedro/Cedar/CedarApplication.java
@@ -0,0 +1,13 @@
+package io.github.CarolinaCedro.Cedar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CedarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CedarApplication.class, args);
+	}
+
+}

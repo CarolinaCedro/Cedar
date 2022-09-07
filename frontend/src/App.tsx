e771964fa7b106efbd73ea001523108984a2df93
CarolinaCedro <yotechgirl@gmail.com>
2022-09-07 18:53:22 -0300

    ++

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header } from "./Components/Header/Header";
+import {AiFillGithub} from "react-icons/ai";
+import logoIcon from './Assets/logoIcon.svg'
+import { CardSale } from "./Components/CardSale/CardSale";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <section className="logoContainer">
+      <div className="logoContent">
+        <img src={logoIcon}/>
+        <h3>Cedar</h3>
+        <p>Desenvolvido por <AiFillGithub/> <a href="https://github.com/CarolinaCedro">CarolinaCedro</a></p>
+      </div>
+    </section>
+    <CardSale/>
+    </>
+    
   );
 }
 
---
+++ frontend/src/Components/ButtonSend/ButtonSend.tsx
@@ -0,0 +1,11 @@
+
+import "./styles.css"
+export function ButtonSend(){
+    return(
+        <div className="dsmeta-red-btn-container">
+            <div className="dsmeta-red-btn">
+            <img src="notification-icon.svg" alt="Notificar" />
+            </div>
+        </div>
+    )
+}
